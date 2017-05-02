04e08a53b94a0cf48ed4368aa3826f1c43a95155
Kihoon Han <khhan1993@gmail.com>
2017-05-02 21:34:14 +0900

    대기열 정보 response 형태 변경 및 'action' argument 삭제

--- app/resources/queue.py
+++ app/resources/queue.py
@@ -47,10 +47,11 @@ class Queue(Resource):
         for each_queue_data in queue_data:
             queue_list[int(each_queue_data['menu_id'])].append(each_queue_data)
 
-        return {
-            "menu_list": menu_list,
-            "queue": queue_list
-        }, 200
+        combined_queue_list = menu_list
+        for each_menu in combined_queue_list:
+            each_menu['queue'] = queue_list[each_menu['id']]
+
+        return combined_queue_list, 200
 
     def put(self):
         if self.user_info is None:
@@ -69,49 +70,39 @@ class Queue(Resource):
         if 'set_reference_id' not in body:
             return {"message": "'set_reference_id' not provided!"}, 400
 
-        if 'action' not in body:
-            return {"message": "'action' not provided!"}, 400
-
         order_id = int(body['order_id'])
         menu_id = int(body['menu_id'])
-        action = body['action']
         set_reference_id = int(body['set_reference_id'])
 
         with db_engine.connect() as connection:
-            if action == 'deliver':
-                query_str = "SELECT * FROM `order_transactions` " \
-                            "WHERE `order_id` = :order_id AND `menu_id` = :menu_id " \
-                            "AND `set_reference_id` = :set_reference_id"
-                chk_queue = connection.execute(text(query_str),
-                                               order_id=order_id, menu_id=menu_id,
-                                               set_reference_id=set_reference_id).first()
+            query_str = "SELECT * FROM `order_transactions` " \
+                        "WHERE `order_id` = :order_id AND `menu_id` = :menu_id " \
+                        "AND `set_reference_id` = :set_reference_id"
+            chk_queue = connection.execute(text(query_str),
+                                           order_id=order_id, menu_id=menu_id,
+                                           set_reference_id=set_reference_id).first()
 
-                if chk_queue is None:
-                    return {"message": "Requested queue('order_id', 'menu_id') not exists!"}, 404
+            if chk_queue is None:
+                return {"message": "Requested queue('order_id', 'menu_id') not exists!"}, 404
 
-                group_id = chk_queue['group_id']
+            group_id = chk_queue['group_id']
 
-                query_str = "SELECT * FROM `members` " \
-                            "WHERE `group_id` = :group_id AND `user_id` = :user_id AND `role` > 0"
-                chk_permission = connection.execute(text(query_str),
-                                                    group_id=group_id, user_id=self.user_info['id']).first()
+            query_str = "SELECT * FROM `members` " \
+                        "WHERE `group_id` = :group_id AND `user_id` = :user_id AND `role` > 0"
+            chk_permission = connection.execute(text(query_str),
+                                                group_id=group_id, user_id=self.user_info['id']).first()
 
-                if chk_permission is None:
-                    return {"message": "You can't update queue status!"}, 403
+            if chk_permission is None:
+                return {"message": "You can't update queue status!"}, 403
 
-                query_str = "UPDATE `order_transactions` SET `is_delivered` = 1, `updated_at` = :cur_time " \
-                            "WHERE `order_id` = :order_id AND `menu_id` = :menu_id " \
-                            "AND `set_reference_id` = :set_reference_id"
-                query = connection.execute(text(query_str), cur_time=datetime.utcnow(),
-                                           order_id=order_id, menu_id=menu_id, set_reference_id=set_reference_id)
-
-            elif action == 'cancel':
-                return {"message": "'action' for 'cancel' is not implemented yet!"}, 501
-            else:
-                return {"message": "'action' must be either 'deliver' or 'cancel'!"}, 400
+            query_str = "UPDATE `order_transactions` SET `is_delivered` = 1, `updated_at` = :cur_time " \
+                        "WHERE `order_id` = :order_id AND `menu_id` = :menu_id " \
+                        "AND `set_reference_id` = :set_reference_id"
+            query = connection.execute(text(query_str), cur_time=datetime.utcnow(),
+                                       order_id=order_id, menu_id=menu_id, set_reference_id=set_reference_id)
 
         return {
             "order_id": order_id,
             "menu_id": menu_id,
-            "action": action
+            "set_reference_id": set_reference_id
         }, 200
