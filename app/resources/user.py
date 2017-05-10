9f5589894757214fa01c60e594389c8994146b8e
Kihoon Han <khhan1993@gmail.com>
2017-05-10 16:10:21 +0900

    Kakao 로그인 추가

--- app/resources/user.py
+++ app/resources/user.py
@@ -9,10 +9,9 @@ import requests
 import json
 
 
-ns = api.namespace('user', description="유저 관련 API (Facebook 로그인, Facebook Application ID : 433842833659960)")
+ns = api.namespace('user', description="유저 관련 API (Facebook/Kakao 로그인)")
 user_post_payload = api.model('User_Post_Payload', {
-    'userID': fields.String("Facebook User ID"),
-    'accessToken': fields.String("Facebook Access Token")
+    'accessToken': fields.String("Facebook/Kakao Access Token")
 })
 
 
@@ -29,64 +28,33 @@ class User(Resource):
         if body is None:
             return {"message": "Unable to get json post data!"}, 400
 
-        if request.args['type'] == 'signup':
-            """
-            if 'name' not in body:
-                return {"message": "'name' not provided!"}, 400
+        if request.args['type'] == 'kakao':
+            if 'access_token' not in body:
+                return {"message": "'access_token' not provided!"}, 400
 
-            if 'email' not in body:
-                return {"message": "'email' not provided!"}, 400
+            kakao_access_token = body['accessToken']
 
-            if 'password' not in body:
-                return {"message": "'password' not provided!"}, 400
+            headers = {"Authorization": "Bearer " + kakao_access_token}
+            res = requests.get("https://kapi.kakao.com/v1/user/me", headers=headers)
+            status_code = res.status_code
+            content = json.loads(res.text)
 
-            email_validator = lepl.apps.rfc3696.Email()
-            if not email_validator(body['email']):
-                return {"message": "Provided email is invalid!"}, 400
-
-            if len(body['name']) > 64:
-                return {"message": "Length of 'name' must be smaller than 64!"}, 400
-
-            if len(body['email']) > 64:
-                return {"message": "Length of 'email' must be smaller than 64!"}, 400
-
-            name = body['name']
-            email = body['email']
-            password = pbkdf2_sha256.hash(body['password'])
+            if not res.ok:
+                return {"message": content['msg']}, status_code
 
             with db_engine.connect() as connection:
-                query_str = "SELECT * FROM `users` WHERE `email` = :email"
-                chk_user = connection.execute(text(query_str), email=body['email']).first()
+                query_str = "SELECT * FROM `users` WHERE `kakako_id` = :kakako_id"
+                chk_user = connection.execute(text(query_str), kakako_id=content['id']).first()
 
-                if chk_user is not None:
-                    return {"message": "Email already exists!"}, 403
+                if chk_user is None:
+                    with connection.begin() as transaction:
+                        query_str = "INSERT INTO `users` SET `name` = :name, `kakako_id` = :kakako_id, " \
+                                    "`verified` = 1, `created_at` = :cur_time, `updated_at` = :cur_time"
+                        query = connection.execute(text(query_str), name=content['properties']['nickname'],
+                                                   kakako_id=content['id'], cur_time=datetime.utcnow())
 
-                with connection.begin() as transaction:
-                    query_str = "INSERT INTO `users` SET `name`= :name, `email` = :email, `password` = :password, " \
-                                "`created_at` = :cur_time, `updated_at` = :cur_time"
-                    query = connection.execute(text(query_str),
-                                               name=name, email=email, password=password, cur_time=datetime.utcnow())
-
-                    new_user_id = query.lastrowid
-
-            return {"user_id": new_user_id}, 201
-            """
-            return {"message": "Please use Social authentication!"}, 503
-
-        elif request.args['type'] == 'signin':
-            """
-                        if 'email' not in body:
-                return {"message": "'email' not provided!"}, 400
-
-            if 'password' not in body:
-                return {"message": "'password' not provided!"}, 400
-
-            with db_engine.connect() as connection:
-                query_str = "SELECT * FROM `users` WHERE `email` = :email"
-                chk_user = connection.execute(text(query_str), email=body['email']).first()
-
-                if chk_user is None or pbkdf2_sha256.verify(body['password'], chk_user['password']) is False:
-                    return {"message": "Wrong ID or Password!"}, 403
+                    query_str = "SELECT * FROM `users` WHERE `kakako_id` = :kakako_id"
+                    chk_user = connection.execute(text(query_str), kakako_id=content['id']).first()
 
                 if chk_user['enabled'] != 1:
                     return {"message": "This account has been disabled. Please contact system administrator!"}
@@ -95,12 +63,9 @@ class User(Resource):
                 "jwt": jwt.encode({
                     'user_id': chk_user['id'],
                     'user_name': chk_user['name'],
-                    'user_email': chk_user['email'],
                     'exp': int(time()) + 86400
                 }, key=app.config['JWT_SECRET_KEY'], algorithm='HS512').decode('utf-8')
             }, 200
-            """
-            return {"message": "Please use Social authentication!"}, 503
 
         elif request.args['type'] == 'facebook':
             if 'userID' not in body:
@@ -109,7 +74,6 @@ class User(Resource):
             if 'accessToken' not in body:
                 return {"message": "'accessToken' not provided!"}, 400
 
-            fb_user_id = body['userID']
             fb_access_token = body['accessToken']
 
             params = {"access_token": fb_access_token, "fields": "id,name"}
@@ -120,22 +84,19 @@ class User(Resource):
             if not res.ok:
                 return content['error'], status_code
 
-            if content['id'] != fb_user_id:
-                return {"message": "Provided 'userID' does not match with 'accessToken'!"}, 400
-
             with db_engine.connect() as connection:
                 query_str = "SELECT * FROM `users` WHERE `fb_id` = :fb_id"
-                chk_user = connection.execute(text(query_str), fb_id=fb_user_id).first()
+                chk_user = connection.execute(text(query_str), fb_id=content['id']).first()
 
                 if chk_user is None:
                     with connection.begin() as transaction:
                         query_str = "INSERT INTO `users` SET `name` = :name, `fb_id` = :fb_id, `verified` = 1, " \
                                     "`created_at` = :cur_time, `updated_at` = :cur_time"
                         query = connection.execute(text(query_str), name=content['name'],
-                                                   fb_id=fb_user_id, cur_time=datetime.utcnow())
+                                                   fb_id=content['id'], cur_time=datetime.utcnow())
 
                     query_str = "SELECT * FROM `users` WHERE `fb_id` = :fb_id"
-                    chk_user = connection.execute(text(query_str), fb_id=fb_user_id).first()
+                    chk_user = connection.execute(text(query_str), fb_id=content['id']).first()
 
                 if chk_user['enabled'] != 1:
                     return {"message": "This account has been disabled. Please contact system administrator!"}
@@ -144,7 +105,6 @@ class User(Resource):
                 "jwt": jwt.encode({
                     'user_id': chk_user['id'],
                     'user_name': chk_user['name'],
-                    'user_email': chk_user['email'],
                     'exp': int(time()) + 86400
                 }, key=app.config['JWT_SECRET_KEY'], algorithm='HS512').decode('utf-8')
             }, 200
