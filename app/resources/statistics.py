680b27c58f431b006eb3d00226210172f4e36e65
Kihoon Han <khhan1993@gmail.com>
2017-05-19 23:20:53 +0900

    시간대별 주문 수 현황 Public용 코드 추가

--- app/resources/statistics.py
+++ app/resources/statistics.py
@@ -29,62 +29,75 @@ statistics_get_success = api.model('Statistics_Get_Success', {
 
 
 @ns.route('')
-@ns.param("jwt", "로그인 시 얻은 JWT를 입력", _in="query", required=True)
+@ns.param("jwt", "로그인 시 얻은 JWT를 입력 (group_id 를 명시할 경우는 필수)", _in="query", required=False)
 class Statistics(Resource):
-    @ns.param("group_id", "그룹 고유번호", _in="query", required=True)
+    @ns.param("group_id", "그룹 고유번호, 비워둘 경우 시간대별 주문 현황만 가져옴.", _in="query", required=False)
     @ns.response(200, "통계 정보를 가져온다.", model=statistics_get_success)
     def get(self):
-        if request.user_info is None:
-            return {"message": "JWT must be provided!"}, 401
+        group_id = None
+        if 'group_id' in request.args and request.args['group_id'] != "":
+            group_id = int(request.args['group_id'])
 
-        if 'group_id' not in request.args:
-            return {"message": "'group_id' must be provided!"}, 401
-
-        group_id = int(request.args['group_id'])
+            if request.user_info is None:
+                return {"message": "JWT must be provided!"}, 401
 
         with db_engine.connect() as connection:
-            query_str = "SELECT * FROM `members` WHERE `group_id` = :group_id AND `user_id` = :user_id"
-            chk_member = connection.execute(text(query_str), group_id=group_id, user_id=request.user_info['id']).first()
+            if group_id is not None:
+                query_str = "SELECT * FROM `members` WHERE `group_id` = :group_id AND `user_id` = :user_id"
+                chk_member = connection.execute(text(query_str), group_id=group_id, user_id=request.user_info['id']).first()
 
-            if chk_member is None:
-                return {"message": "Only member of this group can view menu list!"}, 403
+                if chk_member is None:
+                    return {"message": "Only member of this group can get statistics!"}, 403
 
-            query_str = "SELECT `menu_id`, SUM(`amount`) AS `cnt` FROM `order_transactions` " \
-                        "WHERE `group_id` = :group_id AND `is_approved` = 1 GROUP BY `menu_id`"
-            query = connection.execute(text(query_str), group_id=group_id)
-            sales_per_menu = [dict(row) for row in query]
+                query_str = "SELECT `menu_id`, SUM(`amount`) AS `cnt` FROM `order_transactions` " \
+                            "WHERE `group_id` = :group_id AND `is_approved` = 1 GROUP BY `menu_id`"
+                query = connection.execute(text(query_str), group_id=group_id)
+                sales_per_menu = [dict(row) for row in query]
 
-            query_str = "SELECT `menu_id`, AVG(UNIX_TIMESTAMP(`updated_at`) - UNIX_TIMESTAMP(`created_at`)) " \
-                        "AS `avg_delay` FROM `order_transactions` " \
-                        "WHERE `group_id` = :group_id AND `is_delivered` = 1 GROUP BY `menu_id`"
-            query = connection.execute(text(query_str), group_id=group_id)
-            delays_per_menu = [dict(row) for row in query]
+                query_str = "SELECT `menu_id`, AVG(UNIX_TIMESTAMP(`updated_at`) - UNIX_TIMESTAMP(`created_at`)) " \
+                            "AS `avg_delay` FROM `order_transactions` " \
+                            "WHERE `group_id` = :group_id AND `is_delivered` = 1 GROUP BY `menu_id`"
+                query = connection.execute(text(query_str), group_id=group_id)
+                delays_per_menu = [dict(row) for row in query]
 
-            query_str = "SELECT SUM(`total_price`) AS `total_sales_price` FROM `orders` " \
-                        "WHERE `group_id` = :group_id AND `status` = 1"
-            query = connection.execute(text(query_str), group_id=group_id).first()
-            total_sales_price = int(query['total_sales_price']) if query['total_sales_price'] is not None else 0
+                query_str = "SELECT SUM(`total_price`) AS `total_sales_price` FROM `orders` " \
+                            "WHERE `group_id` = :group_id AND `status` = 1"
+                query = connection.execute(text(query_str), group_id=group_id).first()
+                total_sales_price = int(query['total_sales_price']) if query['total_sales_price'] is not None else 0
 
-            query_str = "SELECT `users`.`name`, `orders`.`user_id`, COUNT(`orders`.`id`) AS `cnt` FROM `orders` " \
-                        "JOIN `users` ON `users`.`id` = `orders`.`user_id` " \
-                        "WHERE `orders`.`group_id` = :group_id AND `orders`.`status` = 1 " \
-                        "GROUP BY `orders`.`user_id` " \
-                        "ORDER BY `cnt` DESC"
-            query = connection.execute(text(query_str), group_id=group_id)
-            order_rank_list = [dict(row) for row in query]
+                query_str = "SELECT `users`.`name`, `orders`.`user_id`, COUNT(`orders`.`id`) AS `cnt` FROM `orders` " \
+                            "JOIN `users` ON `users`.`id` = `orders`.`user_id` " \
+                            "WHERE `orders`.`group_id` = :group_id AND `orders`.`status` = 1 " \
+                            "GROUP BY `orders`.`user_id` " \
+                            "ORDER BY `cnt` DESC"
+                query = connection.execute(text(query_str), group_id=group_id)
+                order_rank_list = [dict(row) for row in query]
 
-            query_str = "SELECT CAST(((UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(`created_at`)) / 3600) AS UNSIGNED) " \
-                        "AS `time_elapsed`, COUNT(`id`) AS `cnt` FROM `orders` " \
-                        "WHERE `group_id` = :group_id AND `status` = 1 " \
-                        "AND UNIX_TIMESTAMP(`created_at`) > (UNIX_TIMESTAMP(NOW()) - 86400) " \
-                        "GROUP BY `time_elapsed`"
-            query = connection.execute(text(query_str), group_id=group_id)
-            order_status_per_hour = [dict(row) for row in query]
+                query_str = "SELECT CAST(((UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(`created_at`)) / 3600) AS UNSIGNED) " \
+                            "AS `time_elapsed`, COUNT(`id`) AS `cnt` FROM `orders` " \
+                            "WHERE `group_id` = :group_id AND `status` = 1 " \
+                            "AND UNIX_TIMESTAMP(`created_at`) > (UNIX_TIMESTAMP(NOW()) - 86400) " \
+                            "GROUP BY `time_elapsed`"
+                query = connection.execute(text(query_str), group_id=group_id)
+                order_status_per_hour = [dict(row) for row in query]
 
-        return json.loads(str(json.dumps({
-            "sales_per_menu": sales_per_menu,
-            "delays_per_menu": delays_per_menu,
-            "total_sales_price": total_sales_price,
-            "order_rank_list": order_rank_list,
-            "order_status_per_hour": order_status_per_hour
-        }, default=helper.json_serial))), 200
+                result = json.loads(str(json.dumps({
+                    "sales_per_menu": sales_per_menu,
+                    "delays_per_menu": delays_per_menu,
+                    "total_sales_price": total_sales_price,
+                    "order_rank_list": order_rank_list,
+                    "order_status_per_hour": order_status_per_hour
+                }, default=helper.json_serial)))
+            else:
+                query_str = "SELECT CAST(((UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(`created_at`)) / 3600) AS UNSIGNED) " \
+                            "AS `time_elapsed`, COUNT(`id`) AS `cnt` FROM `orders` " \
+                            "WHERE `status` = 1 AND UNIX_TIMESTAMP(`created_at`) > (UNIX_TIMESTAMP(NOW()) - 86400) " \
+                            "GROUP BY `time_elapsed`"
+                query = connection.execute(text(query_str))
+                order_status_per_hour = [dict(row) for row in query]
+
+                result = json.loads(str(json.dumps({
+                    "order_status_per_hour": order_status_per_hour
+                }, default=helper.json_serial)))
+
+        return result, 200
